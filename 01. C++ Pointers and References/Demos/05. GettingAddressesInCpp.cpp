c9780977f0a9a04ede5facbfc67ce13b9b0a07e3
MartinBG <MartinBG@abv.bg>
2018-09-10 15:11:27 +0300

    Updated demo to print function addresses: https://stackoverflow.com/questions/2064692/how-to-print-function-pointers-with-cout

--- 01. C++ Pointers and References/Demos/05. GettingAddressesInCpp.cpp
+++ 01. C++ Pointers and References/Demos/05. GettingAddressesInCpp.cpp
@@ -12,10 +12,12 @@ int main() {
 	cout << x << " at " << addressX << endl;
 
 	cout << "f()" << " code at " << &f << endl;
+	cout << "f()" << " code at " << (void*)&f << endl; // Use (void*) to print function address
 
 	// NOTE: there are different abs functions (int, double...), so we specify we want the one returning double and accepting 1 double parameter
 	double(&absAddress)(double) = abs;
 	cout << "abs(double)" << " code at " << absAddress << endl;
+	cout << "abs(double)" << " code at " << (void*)absAddress << endl; // Use (void*) to print function address
 
 	return 0;
 }
