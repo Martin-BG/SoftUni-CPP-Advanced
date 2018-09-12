e963e2b553b62f58b9a1efab973b5100db355468
MartinBG <MartinBG@abv.bg>
2018-09-12 20:03:23 +0300

    done task 1

--- 02. Memory Management/Homework/01. MinBy/MinBy.h
+++ 02. Memory Management/Homework/01. MinBy/MinBy.h
@@ -1,4 +1,15 @@
 #ifndef CPP_ADVANCED_MINBY_H
 #define CPP_ADVANCED_MINBY_H
 
+#include <string>
+#include <vector>
+#include <algorithm>
+
+typedef bool(* comparator)(const std::string&, const std::string&);
+
+std::string minBy(std::vector<std::string>& input, const comparator comp) {
+  std::sort(input.begin(), input.end(), comp);
+  return input.at(0);
+}
+
 #endif //CPP_ADVANCED_MINBY_H
