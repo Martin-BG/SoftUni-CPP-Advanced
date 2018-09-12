ffd833dc0c6b013eaf8b1ef533acbfac17f73245
MartinBG <MartinBG@abv.bg>
2018-09-13 00:27:24 +0300

    done task 2

--- 02. Memory Management/Homework/02. Remove Invalid/RemoveInvalid.h
+++ 02. Memory Management/Homework/02. Remove Invalid/RemoveInvalid.h
@@ -1,4 +1,34 @@
 #ifndef CPP_ADVANCED_REMOVEINVALID_H
 #define CPP_ADVANCED_REMOVEINVALID_H
 
+#include <list>
+#include <algorithm>
+#include "Company.h"
+
+namespace local {
+  bool removed(Company* company) {
+    if (company->getId() < 0) {
+      delete company;
+      return true;
+    }
+    return false;
+  }
+}
+
+void removeInvalid(std::list<Company*>& companies) {
+  companies.remove_if(local::removed);
+}
+
+/* //Solution using lambda
+void removeInvalid(std::list<Company*>& companies) {
+  companies.remove_if([](Company* company) {
+    if (company->getId() < 0) {
+      delete company;
+      return true;
+    }
+    return false;
+  });
+}
+*/
+
 #endif //CPP_ADVANCED_REMOVEINVALID_H
