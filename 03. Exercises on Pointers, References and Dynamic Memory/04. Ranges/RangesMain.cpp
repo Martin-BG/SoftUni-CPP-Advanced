9bf95f7240ee81bdab6df0b152eb5414804f1d60
MartinBG <MartinBG@abv.bg>
2018-09-17 22:55:43 +0300

    done task 4

---
+++ 03. Exercises on Pointers, References and Dynamic Memory/04. Ranges/RangesMain.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <map>
+
+
+int main() {
+  std::ostream::sync_with_stdio(false);
+  std::istream::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  std::map<int, int> starts;
+  std::map<int, int> ends;
+
+  int rangeId = 0;
+  std::string line;
+  while (getline(std::cin, line) && line != ".") {
+    std::istringstream lineIn(line);
+    int from, to;
+    lineIn >> from >> to;
+    starts[from] = rangeId;
+    ends[to] = rangeId;
+    ++rangeId;
+  }
+
+  std::ostringstream result;
+  while (getline(std::cin, line) && line != ".") {
+    std::istringstream lineIn(line);
+    int num;
+    lineIn >> num;
+    auto start = starts.upper_bound(num);
+    auto end = ends.lower_bound(num);
+    if (start != starts.begin() && ((--start)->second == end->second)) {
+      result << "in" << std::endl;
+    } else {
+      result << "out" << std::endl;
+    }
+  }
+
+  std::cout << result.str();
+}
