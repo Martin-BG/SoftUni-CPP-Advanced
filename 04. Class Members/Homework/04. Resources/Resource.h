fbf3b54d3f7d51ff56cbcd3d07552c91ced2e5a6
MartinBG <MartinBG@abv.bg>
2018-09-27 06:17:51 +0300

    done task 4

--- 04. Class Members/Homework/04. Resources/Resource.h
+++ 04. Class Members/Homework/04. Resources/Resource.h
@@ -1,8 +1,51 @@
 #ifndef CPP_ADVANCED_RESOURCE_H
 #define CPP_ADVANCED_RESOURCE_H
+
+#include <ostream>
+#include <string>
+#include <sstream>
+#include "ResourceType.h"
+
 namespace SoftUni {
   class Resource {
+    int id{ };
+    ResourceType type;
+    std::string link;
+  public:
+    Resource() = default;;
 
+    const ResourceType getType() const {
+      return this->type;
+    }
+
+    bool operator<(const Resource& other) const {
+      return this->id < other.id;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Resource& resource);
+
+    friend std::istream& operator>>(std::istream& in, Resource& resource);
   };
+
+  std::ostream& operator<<(std::ostream& out, const Resource& resource) {
+    return out << resource.id << " " << resource.type << " " << resource.link;
+  }
+
+  std::istream& operator>>(std::istream& in, ResourceType& resourceType) {
+    std::string type;
+    in >> type;
+    if (type == "Presentation") {
+      resourceType = ResourceType::PRESENTATION;
+    } else if (type == "Demo") {
+      resourceType = ResourceType::DEMO;
+    } else if (type == "Video") {
+      resourceType = ResourceType::VIDEO;
+    }
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, Resource& resource) {
+    return in >> resource.id >> resource.type >> resource.link;
+  }
 }
 #endif //CPP_ADVANCED_RESOURCE_H
