d15c7df3f1591da62932372a6d2ebb4d2964c29c
MartinBG <MartinBG@abv.bg>
2018-09-27 08:09:16 +0300

    done task 5

--- 04. Class Members/Homework/05. Lectures/Lecture.h
+++ 04. Class Members/Homework/05. Lectures/Lecture.h
@@ -1,10 +1,50 @@
 #ifndef CPP_ADVANCED_LECTURE_H
 #define CPP_ADVANCED_LECTURE_H
 
-namespace SoftUni {
-  class Lecture {
+#include <set>
+#include <vector>
+#include "Resource.h"
 
+namespace SoftUni {
+  static const int RESOURCE_TYPES = 3;
+
+  class Lecture {
+    std::set<Resource> resources;
+    int resourcesByType[RESOURCE_TYPES]{ };
+  public:
+    Lecture() = default;
+
+    Lecture& operator<<(const Resource& resource) {
+      if (this->resources.erase(resource) == 0) {
+        ++this->resourcesByType[resource.getType()];
+      }
+      this->resources.insert(resource);
+      return *this;
+    }
+
+    int operator[](const ResourceType& type) const {
+      return this->resourcesByType[type];
+    }
+
+    std::set<Resource>::iterator begin() const {
+      return this->resources.begin();
+    };
+
+    std::set<Resource>::iterator end() const {
+      return this->resources.end();
+    };
+
+    friend std::vector<ResourceType>& operator<<(std::vector<ResourceType>& usedResources, const Lecture& lecture);
   };
+
+  std::vector<ResourceType>& operator<<(std::vector<ResourceType>& usedResources, const Lecture& lecture) {
+    for (int i = 0; i < RESOURCE_TYPES; ++i) {
+      if (lecture.resourcesByType[i]) {
+        usedResources.push_back((ResourceType) i);
+      }
+    }
+    return usedResources;
+  }
 }
 
 #endif //CPP_ADVANCED_LECTURE_H
--- 04. Class Members/Homework/05. Lectures/Resource.h
+++ 04. Class Members/Homework/05. Lectures/Resource.h
@@ -1,10 +1,64 @@
 #ifndef CPP_ADVANCED_RESOURCE_H
 #define CPP_ADVANCED_RESOURCE_H
 
+#include <ostream>
+#include <string>
+#include <sstream>
+#include "ResourceType.h"
+
 namespace SoftUni {
   class Resource {
+    int id{ };
+    ResourceType type{ };
+    std::string link;
+  public:
+    Resource() = default;;
 
+    int getId() const {
+      return this->id;
+    }
+
+    ResourceType getType() const {
+      return this->type;
+    }
+
+    const std::string& getLink() const {
+      return this->link;
+    }
+
+    bool operator<(const Resource& other) const {
+      return this->id < other.id;
+    }
+
+    bool operator==(const Resource& other) const {
+      return this->id == other.id;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Resource& resource);
+
+    friend std::istream& operator>>(std::istream& in, Resource& resource);
   };
+
+  std::ostream& operator<<(std::ostream& out, const Resource& resource) {
+    return out << resource.id << " " << resource.type << " " << resource.link;
+  }
+
+  std::istream& operator>>(std::istream& in, ResourceType& resourceType) {
+    std::string type;
+    in >> type;
+    if (type == "Presentation") {
+      resourceType = ResourceType::PRESENTATION;
+    } else if (type == "Demo") {
+      resourceType = ResourceType::DEMO;
+    } else if (type == "Video") {
+      resourceType = ResourceType::VIDEO;
+    }
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, Resource& resource) {
+    return in >> resource.id >> resource.type >> resource.link;
+  }
 }
 
 #endif //CPP_ADVANCED_RESOURCE_H
