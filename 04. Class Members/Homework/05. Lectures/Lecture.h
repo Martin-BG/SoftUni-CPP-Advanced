9c5e26d6a43be02b59e683aec05d8550ffc4b96b
MartinBG <MartinBG@abv.bg>
2018-09-27 04:10:27 +0300

    placeholders

---
+++ 04. Class Members/Homework/05. Lectures/Lecture.h
@@ -0,0 +1,10 @@
+#ifndef CPP_ADVANCED_LECTURE_H
+#define CPP_ADVANCED_LECTURE_H
+
+namespace SoftUni {
+  class Lecture {
+
+  };
+}
+
+#endif //CPP_ADVANCED_LECTURE_H
---
+++ 04. Class Members/Homework/05. Lectures/Resource.h
@@ -0,0 +1,10 @@
+#ifndef CPP_ADVANCED_RESOURCE_H
+#define CPP_ADVANCED_RESOURCE_H
+
+namespace SoftUni {
+  class Resource {
+
+  };
+}
+
+#endif //CPP_ADVANCED_RESOURCE_H
