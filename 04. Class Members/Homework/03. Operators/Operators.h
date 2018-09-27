12ff0144a00ce2f21472dd5655bfaa5e18001b88
MartinBG <MartinBG@abv.bg>
2018-09-27 05:33:40 +0300

    done task 3

--- 04. Class Members/Homework/03. Operators/Operators.h
+++ 04. Class Members/Homework/03. Operators/Operators.h
@@ -1,4 +1,29 @@
 #ifndef CPP_ADVANCED_OPERATORS_H
 #define CPP_ADVANCED_OPERATORS_H
 
+#include <ostream>
+#include <vector>
+#include <string>
+#include <sstream>
+
+typedef std::vector<std::string> vectorOfStrings;
+
+vectorOfStrings& operator<<(vectorOfStrings& vector, const std::string& str) {
+  vector.push_back(str);
+  return vector;
+}
+
+std::ostream& operator<<(std::ostream& out, const vectorOfStrings& vector) {
+  for (const auto& line : vector) {
+    out << line << std::endl;
+  }
+  return out;
+}
+
+std::string operator+(const std::string& str, const int& num) {
+  std::ostringstream os;
+  os << str << num;
+  return os.str();
+}
+
 #endif //CPP_ADVANCED_OPERATORS_H
