d9f7e5328a3f9750acf44430c7427314cf67a495
MartinBG <MartinBG@abv.bg>
2018-09-27 05:02:06 +0300

    done task 2

--- 04. Class Members/Homework/02. Sum Of Vectors/SumOfVectors.h
+++ 04. Class Members/Homework/02. Sum Of Vectors/SumOfVectors.h
@@ -1,4 +1,17 @@
 #ifndef CPP_ADVANCED_SUMOFVECTORS_H
 #define CPP_ADVANCED_SUMOFVECTORS_H
 
+#include <vector>
+#include <string>
+
+typedef std::vector<std::string> vectorOfStrings;
+
+vectorOfStrings operator+(const vectorOfStrings& v1, const vectorOfStrings& v2) {
+  vectorOfStrings result;
+  for (size_t i = 0; i < v1.size() && i < v2.size(); ++i) {
+    result.push_back(v1.at(i) + " " + v2.at(i));
+  }
+  return result;
+}
+
 #endif //CPP_ADVANCED_SUMOFVECTORS_H
