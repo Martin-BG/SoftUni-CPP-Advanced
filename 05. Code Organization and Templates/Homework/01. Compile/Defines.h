a3ef70fc7617be4aadeac0b53d78f1251a66fd0b
MartinBG <MartinBG@abv.bg>
2018-10-03 05:15:17 +0300

    done task 1

---
+++ 05. Code Organization and Templates/Homework/01. Compile/Defines.h
@@ -0,0 +1,10 @@
+#ifndef CPP_ADVANCED_DEFINES_H
+#define CPP_ADVANCED_DEFINES_H
+
+#define DONT_COMPILE_THIS
+#define STANDARD_TEMPLATE_LIBRARY namespace std;
+
+#include <string>
+#include <iostream>
+
+#endif //CPP_ADVANCED_DEFINES_H
