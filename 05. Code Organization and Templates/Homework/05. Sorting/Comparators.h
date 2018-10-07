98d90c62dab131e8473314920cda9af6b446cd67
MartinBG <MartinBG@abv.bg>
2018-10-08 02:06:26 +0300

    added proper(not hackish) solution for the problem refactored the original solution (use of templates)

--- 05. Code Organization and Templates/Homework/05. Sorting/Comparators.h
+++ 05. Code Organization and Templates/Homework/05. Sorting/Comparators.h
@@ -5,23 +5,34 @@
 #include <iostream>
 #include "Song.h"
 
+template<typename T>
+class LessThan {
+public:
+  static bool evaluate(const T& lhs, const T& rhs) {
+    return lhs < rhs;
+  };
+};
+
+template<typename T, typename D>
+class Reverse {
+public:
+  bool operator()(const T& lhs, const T& rhs) const {
+    return !D::evaluate(lhs, rhs);
+  }
+};
+
+/*
+// Solution #1 - the hackish one
+template<typename T>
 struct comparator {
-  bool operator()(const int& lhs, const int& rhs) const {
-    return lhs >= rhs;
-  }
-
-  bool operator()(const std::string& lhs, const std::string& rhs) const {
-    return lhs >= rhs;
-  }
-
-  bool operator()(const Song& lhs, const Song& rhs) const {
-    return lhs.getLengthSeconds() >= rhs.getLengthSeconds();
+  bool operator()(const T& lhs, const T& rhs) const {
+    return !(lhs < rhs);
   }
 };
 
 template<class T>
-using LessThan = comparator;
+using LessThan = comparator<T>;
 template<typename T, typename = LessThan<T> >
 using Reverse = LessThan<T>;
-
+*/
 #endif //CPP_ADVANCED_COMPARATORS_H
