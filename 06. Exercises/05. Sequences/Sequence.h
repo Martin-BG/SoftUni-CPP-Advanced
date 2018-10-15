285d5b7aa45320197b6d77c2da54fa45c824a64c
MartinBG <MartinBG@abv.bg>
2018-10-16 00:15:05 +0300

    refactoring - Moved Iterator class out of Sequence class

--- 06. Exercises/05. Sequences/Sequence.h
+++ 06. Exercises/05. Sequences/Sequence.h
@@ -4,62 +4,63 @@
 #include <vector>
 #include <iostream>
 
+template<typename T>
+class Iterator {
+private:
+  static const int END_ITERATOR_INDEX = -1;
+
+  std::vector<T>& elements;
+  int next;
+
+  explicit Iterator(std::vector<T>& elements, int next) : elements(elements), next(next) { }
+
+public:
+  static Iterator begin(std::vector<T>& elements) {
+    if (elements.size() == 0) {
+      return end(elements);
+    }
+    return Iterator(elements, 0);
+  }
+
+  static Iterator end(std::vector<T>& elements) {
+    return Iterator(elements, END_ITERATOR_INDEX);
+  }
+
+  const bool operator!=(const Iterator& other) const {
+    return this->elements != other.elements || this->next != other.next;
+  }
+
+  Iterator& operator++() {
+    ++this->next;
+    if (this->next >= this->elements.size()) {
+      this->next = END_ITERATOR_INDEX;
+    }
+    return *this;
+  }
+
+  T& operator*() const {
+    return this->elements.at(this->next);
+  }
+};
+
 template<typename T, typename Generator>
 class Sequence {
 private:
   Generator generator;
   std::vector<T> elements{ };
 public:
-  class Iterator {
-  private:
-    static const int END_ITERATOR_INDEX = -1;
-    std::vector<T>& elements;
-    int next;
-
-    explicit Iterator(std::vector<T>& elements, int next) : elements(elements), next(next) {
-      if (next >= this->elements.size() || this->elements.size() == 0) {
-        this->next = END_ITERATOR_INDEX;
-      }
-    }
-
-  public:
-    static Iterator end(std::vector<T>& elements) {
-      return Iterator(elements, END_ITERATOR_INDEX);
-    }
-
-    static Iterator begin(std::vector<T>& elements) {
-      return Iterator(elements, 0);
-    }
-
-    const bool operator!=(const Iterator& other) const {
-      return this->elements != other.elements || this->next != other.next;
-    }
-
-    Iterator operator++() {
-      ++this->next;
-      if (this->next >= this->elements.size()) {
-        this->next = END_ITERATOR_INDEX;
-      }
-      return *this;
-    }
-
-    T& operator*() const {
-      return this->elements.at(this->next);
-    }
-  };
-
   void generateNext(int count) {
     for (int i = 0; i < count; ++i) {
       this->elements.push_back(this->generator());
     }
   }
 
-  Iterator begin() {
-    return Iterator::begin(this->elements);
+  Iterator<T> begin() {
+    return Iterator<T>::begin(this->elements);
   }
 
-  Iterator end() {
-    return Iterator::end(this->elements);
+  Iterator<T> end() {
+    return Iterator<T>::end(this->elements);
   }
 };
 
