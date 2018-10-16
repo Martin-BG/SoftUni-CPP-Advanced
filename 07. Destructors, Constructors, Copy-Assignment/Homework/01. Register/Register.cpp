0bfba1bbff7724583ccfbac61367a14803c5d842
MartinBG <MartinBG@abv.bg>
2018-10-17 02:30:58 +0300

    done task 1

--- 07. Destructors, Constructors, Copy-Assignment/Homework/01. Register/Register.cpp
+++ 07. Destructors, Constructors, Copy-Assignment/Homework/01. Register/Register.cpp
@@ -1,4 +1,21 @@
 #include "Register.h"
 
-// TODO
+Register::Register(size_t numCompanies) : companiesArray(new Company[numCompanies]), numAdded(0) { }
 
+void Register::add(const Company& c) {
+  this->companiesArray[numAdded++] = c;
+}
+
+Company Register::get(int companyId) const {
+  for (int i = 0; i < this->numAdded; ++i) {
+    if (this->companiesArray[i].getId() == companyId) {
+      return this->companiesArray[i];
+    }
+  }
+}
+
+Register::~Register() {
+  delete[] this->companiesArray;
+  this->companiesArray = nullptr;
+  numAdded = 0;
+}
