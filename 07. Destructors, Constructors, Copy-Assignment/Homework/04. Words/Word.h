e5d22a735a8a6289f7f628a5c7eca64794b192af
MartinBG <MartinBG@abv.bg>
2018-10-17 02:31:48 +0300

    done task 4

--- 07. Destructors, Constructors, Copy-Assignment/Homework/04. Words/Word.h
+++ 07. Destructors, Constructors, Copy-Assignment/Homework/04. Words/Word.h
@@ -1,6 +1,43 @@
 #ifndef CPP_ADVANCED_WORD_H
 #define CPP_ADVANCED_WORD_H
 
-// TODO
+#include <string>
+#include <unordered_map>
+
+std::unordered_map<std::string, int> words{ };
+
+class Word {
+private:
+  std::string word;
+  bool removeFromWordsOnDestruct;
+public:
+  explicit Word(std::string word) : word(std::move(word)), removeFromWordsOnDestruct(false) {
+    ++words[this->word];
+  }
+
+  Word(const Word& other) : word(other.word), removeFromWordsOnDestruct(true) { }
+
+  ~Word() {
+    if (this->removeFromWordsOnDestruct) {
+      words.erase(this->word);
+    }
+  }
+
+  std::string getWord() const {
+    return this->word;
+  }
+
+  int getCount() const {
+    return words[this->word];
+  }
+
+  Word& operator=(const Word& other) = delete;
+
+  bool operator<(const Word& other) const {
+    return this->word < other.word;
+  }
+
+  friend std::ostream& operator<<(std::ostream& out, const Word& w);
+};
 
 #endif //CPP_ADVANCED_WORD_H
