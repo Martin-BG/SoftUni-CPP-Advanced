2f8d4a672eca0b025c88997d222aa77d0cd13892
MartinBG <MartinBG@abv.bg>
2018-10-17 02:31:17 +0300

    done task 2

--- 07. Destructors, Constructors, Copy-Assignment/Homework/02. Register of Three/Register.cpp
+++ 07. Destructors, Constructors, Copy-Assignment/Homework/02. Register of Three/Register.cpp
@@ -1,4 +1,48 @@
 #include "Register.h"
 
-// TODO
+Register::Register(size_t numCompanies) : companiesArray(new Company[numCompanies]), numAdded(0) { }
 
+void Register::add(const Company& c) {
+  this->companiesArray[numAdded++] = c;
+}
+
+Company Register::get(int companyId) const {
+  for (int i = 0; i < this->numAdded; ++i) {
+    if (this->companiesArray[i].getId() == companyId) {
+      return this->companiesArray[i];
+    }
+  }
+}
+
+Register::~Register() {
+  delete[] this->companiesArray;
+  this->companiesArray = nullptr;
+  numAdded = 0;
+}
+
+Register& Register::operator=(const Register& other) {
+  if (this != &other) {
+    Company* temp = new Company[other.numAdded];
+    for (size_t i = 0; i < other.numAdded; ++i) {
+      temp[i] = other.companiesArray[i];
+    }
+
+    delete[] this->companiesArray;
+
+    this->companiesArray = temp;
+    this->numAdded = other.numAdded;
+  }
+  return *this;
+}
+
+Register::Register(const Register& other) {
+  Company* temp = new Company[other.numAdded];
+  for (size_t i = 0; i < other.numAdded; ++i) {
+    temp[i] = other.companiesArray[i];
+  }
+
+  delete[] this->companiesArray;
+
+  this->companiesArray = temp;
+  this->numAdded = other.numAdded;
+}
