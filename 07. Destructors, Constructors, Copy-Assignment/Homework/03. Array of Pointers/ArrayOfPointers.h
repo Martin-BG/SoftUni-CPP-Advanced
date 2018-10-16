bb571185130f1241c1622ad91e951aa0c87e0e20
MartinBG <MartinBG@abv.bg>
2018-10-17 02:31:33 +0300

    done task 3

--- 07. Destructors, Constructors, Copy-Assignment/Homework/03. Array of Pointers/ArrayOfPointers.h
+++ 07. Destructors, Constructors, Copy-Assignment/Homework/03. Array of Pointers/ArrayOfPointers.h
@@ -1,6 +1,32 @@
 #ifndef CPP_ADVANCED_ARRAYOFPOINTERS_H
 #define CPP_ADVANCED_ARRAYOFPOINTERS_H
 
-// TODO
+#include <vector>
+#include "Company.h"
+
+class ArrayOfPointers {
+private:
+  std::vector<Company*> companies;
+public:
+  ArrayOfPointers() : companies(std::vector<Company*>{ }) { }
+
+  virtual ~ArrayOfPointers() {
+    for (Company* c : this->companies) {
+      delete c;
+    }
+  }
+
+  void add(Company* pCompany) {
+    this->companies.push_back(pCompany);
+  }
+
+  size_t getSize() {
+    return this->companies.size();
+  }
+
+  Company* get(size_t index) {
+    return this->companies.at(index);
+  }
+};
 
 #endif //CPP_ADVANCED_ARRAYOFPOINTERS_H
