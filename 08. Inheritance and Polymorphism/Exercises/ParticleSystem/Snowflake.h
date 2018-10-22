3a80b7aad3e8d614dc36bd65f73c02ea9afe9876
MartinBG <MartinBG@abv.bg>
2018-10-23 02:43:20 +0300

    - explicit lastMovedDown initialization in constructor - include <cstdlib> - required for rand() - added missing "case 0:" in switch

--- 08. Inheritance and Polymorphism/Exercises/ParticleSystem/Snowflake.h
+++ 08. Inheritance and Polymorphism/Exercises/ParticleSystem/Snowflake.h
@@ -1,12 +1,13 @@
 #ifndef SNOWFLAKE_H
 #define SNOWFLAKE_H
 
+#include <cstdlib>
 #include "Particle.h"
 
 class Snowflake : public Particle {
 	bool lastMovedDown;
 public:
-	Snowflake(Position p, int lifetime) : Particle(p, '*', lifetime) {}
+	Snowflake(Position p, int lifetime) : Particle(p, '*', lifetime), lastMovedDown(false) {}
 
 	virtual void update(std::list<Particle*>& producedParticlesOutput) override {
 		Particle::update(producedParticlesOutput);
@@ -17,6 +18,7 @@ public:
 		this->lastMovedDown = !this->lastMovedDown;
 
 		switch (rand() % 2) {
+		case 0:
 			this->position = this->position.prevCol();
 			break;
 		case 1:
