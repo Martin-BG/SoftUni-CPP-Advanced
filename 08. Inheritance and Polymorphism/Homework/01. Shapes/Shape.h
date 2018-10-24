7790f47e631f246756a95a988175021b4d48dfd1
MartinBG <MartinBG@abv.bg>
2018-10-24 05:56:23 +0300

    done task 1

--- 08. Inheritance and Polymorphism/Homework/01. Shapes/Shape.h
+++ 08. Inheritance and Polymorphism/Homework/01. Shapes/Shape.h
@@ -1,8 +1,28 @@
 #ifndef CPP_ADVANCED_SHAPE_H
 #define CPP_ADVANCED_SHAPE_H
 
+#include <iomanip>
 #include "Vector2D.h"
 
-// TODO
+class Shape {
+private:
+  const Vector2D center;
+protected:
+  double area;
+public:
+  Shape() : center(Vector2D(0.0, 0.0)), area(0.0) { };
+
+  explicit Shape(const Vector2D& center) : center(center), area(0.0) { }
+
+  virtual ~Shape() = default;
+
+  std::string getCenter() {
+    return this->center;
+  }
+
+  double getArea() {
+    return this->area;
+  }
+};
 
 #endif //CPP_ADVANCED_SHAPE_H
