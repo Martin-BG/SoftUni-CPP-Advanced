e9e7fd0fc1115422de717ca29efa06003d3710b6
MartinBG <MartinBG@abv.bg>
2018-10-24 16:28:10 +0300

    refactoring

--- 07. Destructors, Constructors, Copy-Assignment/Homework/01. Register/Register.cpp
+++ 07. Destructors, Constructors, Copy-Assignment/Homework/01. Register/Register.cpp
@@ -1,21 +1,21 @@
 #include "Register.h"
 
-Register::Register(size_t numCompanies) : companiesArray(new Company[numCompanies]), numAdded(0) { }
+Register::Register(size_t numCompanies) : numAdded(0), companiesArray(new Company[numCompanies]) { }
 
 void Register::add(const Company& c) {
-  this->companiesArray[numAdded++] = c;
+  this->companiesArray[this->numAdded++] = c;
 }
 
 Company Register::get(int companyId) const {
-  for (int i = 0; i < this->numAdded; ++i) {
+  for (size_t i = 0; i < this->numAdded; ++i) {
     if (this->companiesArray[i].getId() == companyId) {
       return this->companiesArray[i];
     }
   }
+
+  return Company();
 }
 
 Register::~Register() {
   delete[] this->companiesArray;
-  this->companiesArray = nullptr;
-  numAdded = 0;
 }
