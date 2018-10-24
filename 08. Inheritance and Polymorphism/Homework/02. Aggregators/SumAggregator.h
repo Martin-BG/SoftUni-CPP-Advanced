742713ea9c03a7f14651032c15c8441b0711fc34
MartinBG <MartinBG@abv.bg>
2018-10-24 05:56:56 +0300

    done task 2

--- 08. Inheritance and Polymorphism/Homework/02. Aggregators/SumAggregator.h
+++ 08. Inheritance and Polymorphism/Homework/02. Aggregators/SumAggregator.h
@@ -1,6 +1,18 @@
 #ifndef CPP_ADVANCED_SUMAGGREGATOR_H
 #define CPP_ADVANCED_SUMAGGREGATOR_H
 
-// TODO
+#include "Aggregator.h"
+
+class SumAggregator : public StreamAggregator {
+protected:
+  void aggregate(int next) override {
+    this->aggregationResult += next;
+  }
+
+public:
+  explicit SumAggregator(std::istream& input) : StreamAggregator(input) {
+    this->aggregationResult = 0;
+  }
+};
 
 #endif //CPP_ADVANCED_SUMAGGREGATOR_H
