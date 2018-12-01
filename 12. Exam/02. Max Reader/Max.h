2266b0c8fd1c140ff3590abf3dab18c11b7911b4
MartinBG <MartinBG@abv.bg>
2018-12-01 18:18:24 +0200

    task #2 - refactored

--- 12. Exam/02. Max Reader/Max.h
+++ 12. Exam/02. Max Reader/Max.h
@@ -7,6 +7,29 @@
 #include <vector>
 
 class VectorMax {
+  int max{ INT_MIN };
+  std::vector<int> nums{ };
+public:
+  VectorMax() = default;
+
+  friend void operator>>(const std::vector<int>& arr, VectorMax& vectorMax) {
+    vectorMax.nums = arr;
+    vectorMax.max = *std::max_element(vectorMax.nums.begin(), vectorMax.nums.end());
+  }
+
+  friend std::ostream& operator<<(std::ostream& out, const VectorMax& vectorMax) {
+    for (auto const& num : vectorMax.nums) {
+      out << num << ' ';
+    }
+    return out;
+  }
+
+  friend const bool operator<(const VectorMax& lhs, const VectorMax& rhs) {
+    return lhs.max < rhs.max;
+  }
+};
+
+/*class VectorMax {
   std::vector<int> nums{ };
 public:
   VectorMax() = default;
@@ -25,6 +48,6 @@ public:
   friend const bool operator<(const VectorMax& lhs, const VectorMax& rhs) {
     return *std::max_element(lhs.nums.begin(), lhs.nums.end()) < *std::max_element(rhs.nums.begin(), rhs.nums.end());
   }
-};
+};*/
 
 #endif //CPP_ADVANCED_MAX_H
--- 12. Exam/02. Max Reader/Reader.h
+++ 12. Exam/02. Max Reader/Reader.h
@@ -9,6 +9,23 @@ class Reader {
 public:
   explicit Reader(std::istream& in) : in(in) { }
 
+  Reader& operator>>(std::vector<int>& arr) {
+    std::string line;
+    std::getline(this->in >> std::ws, line);
+    std::istringstream is(line);
+    int next;
+    while (is >> next) {
+      arr.emplace_back(next);
+    }
+    return *this;
+  }
+};
+
+/*class Reader {
+  std::istream& in;
+public:
+  explicit Reader(std::istream& in) : in(in) { }
+
   std::vector<int>& operator>>(std::vector<int>& arr) {
     std::string line;
     std::getline(this->in >> std::ws, line);
@@ -19,6 +36,5 @@ public:
     }
     return arr;
   }
-};
-
+};*/
 #endif //CPP_ADVANCED_READER_H
