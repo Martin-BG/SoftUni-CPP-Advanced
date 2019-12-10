bd33f03f639f8d28525d29f4a16db619be43edc0
MartinBG <MartinBG@abv.bg>
2019-12-10 16:39:48 +0200

    solution

--- 14. Exams/2019.04.07/03. Hardware Store/Laptop.cpp
+++ 14. Exams/2019.04.07/03. Hardware Store/Laptop.cpp
@@ -1,1 +1,37 @@
-// TODO
+#include "Laptop.h"
+
+Laptop::Laptop(const std::string& name,
+               const double price,
+               const double monitorSize) :
+    Hardware(name, price),
+    _monitorSize(monitorSize) {
+  this->printInfo();
+  std::cout << " is being created" << std::endl;
+}
+
+Laptop::~Laptop() {
+  this->printInfo();
+  std::cout << " is being destroyed" << std::endl;
+}
+
+Laptop::Laptop(const Laptop& other) :
+    Hardware(other.getName(), other.getPrice()),
+    _monitorSize(other._monitorSize) {
+  std::cout << "Copy construction for ";
+  this->printInfo();
+  std::cout << std::endl;
+}
+
+Laptop& Laptop::operator=(const Laptop& other) {
+  if (this != &other) {
+    Hardware::operator=(other);
+    this->_monitorSize = other._monitorSize;
+    std::cout << "Copy assignment for ";
+  } else {
+    std::cout << "Self-copy prevented for ";
+  }
+  this->printInfo();
+  std::cout << std::endl;
+
+  return *this;
+}
--- 14. Exams/2019.04.07/03. Hardware Store/Store.cpp
+++ 14. Exams/2019.04.07/03. Hardware Store/Store.cpp
@@ -1,1 +1,10 @@
-// TODO
+#include "Store.h"
+
+void Store::remove(const int index) {
+  _laptops.erase(_laptops.begin() + index);
+}
+
+void Store::copy(const int fromIndex, const int toIndex) {
+  _laptops.at(toIndex) = _laptops.at(fromIndex);
+}
+
