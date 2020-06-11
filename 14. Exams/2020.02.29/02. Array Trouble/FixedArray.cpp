9793362a84e5bb35c2e7802be5186dce9f043718
MartinBG <MartinBG@abv.bg>
2020-06-12 00:18:42 +0300

    task #2

--- 14. Exams/2020.02.29/02. Array Trouble/FixedArray.cpp
+++ 14. Exams/2020.02.29/02. Array Trouble/FixedArray.cpp
@@ -1,3 +1,36 @@
+#include <algorithm>
 #include "FixedArray.h"
 
-//TODO
+FixedArray::FixedArray(const int arraySize) : BrokenArray(arraySize) {
+}
+
+FixedArray::~FixedArray() = default;
+
+FixedArray::FixedArray(const FixedArray& other) : BrokenArray(other) {
+  _array = new int[getSize()];
+  std::copy(other._array, other._array + getSize(), _array);
+}
+
+FixedArray& FixedArray::operator=(const FixedArray& other) {
+  if (&other == this) {
+    return *this;
+  }
+
+  int* copy = new int[other.getSize()];
+
+  std::copy(other._array, other._array + other.getSize(), copy);
+
+  delete[] _array;
+  _array = copy;
+  BrokenArray::operator=(other);
+
+  return *this;
+}
+
+void FixedArray::addValueToMemory(const int value) {
+  BrokenArray::addValueToMemory(value);
+}
+
+int FixedArray::getMemorySumValue() const {
+  return BrokenArray::getMemorySumValue();
+}
