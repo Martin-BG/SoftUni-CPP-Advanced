563e2ed2a88f83359f348edff0bd0b7fa79ebc5b
MartinBG <MartinBG@abv.bg>
2020-06-13 16:49:23 +0300

    cleanup

--- 05. Code Organization and Templates/Homework/05. Sorting/Comparators.h
+++ 05. Code Organization and Templates/Homework/05. Sorting/Comparators.h
@@ -7,14 +7,14 @@
 
 template<typename T>
 struct LessThan {
-  const bool operator()(const T& lhs, const T& rhs) const {
+  bool operator()(const T& lhs, const T& rhs) const {
     return lhs < rhs;
   };
 };
 
 template<typename T, typename Comparator>
 struct Reverse {
-  const bool operator()(const T& lhs, const T& rhs) const {
+  bool operator()(const T& lhs, const T& rhs) const {
     Comparator comparator;
     return !comparator(lhs, rhs);
   }
