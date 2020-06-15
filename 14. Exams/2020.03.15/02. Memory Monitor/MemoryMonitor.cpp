7c35ac792abdeb3b5544859afa72e37a26b8eadd
MartinBG <MartinBG@abv.bg>
2020-06-15 10:42:16 +0300

    task #2

--- 14. Exams/2020.03.15/02. Memory Monitor/MemoryMonitor.cpp
+++ 14. Exams/2020.03.15/02. Memory Monitor/MemoryMonitor.cpp
@@ -1,3 +1,52 @@
+#include <iostream>
 #include "MemoryMonitor.h"
 
-//TODO
+MemoryMonitor::MemoryMonitor() { }
+
+MemoryMonitor::~MemoryMonitor() {
+  for (auto& node : _nodes) {
+    delete node.data;
+  }
+}
+
+void MemoryMonitor::pushNode(const int nodeSize) {
+  MemoryNode memoryNode;
+  memoryNode.size = nodeSize;
+  memoryNode.data = new int[nodeSize];
+
+  _nodes.push_back(memoryNode);
+
+  std::cout << "Pushed node with memory occupation: "
+            << nodeSize * sizeof(int) << std::endl;
+}
+
+void MemoryMonitor::popNode() {
+  if (_nodes.empty()) {
+    std::cout << "No nodes to pop" << std::endl;
+    return;
+  }
+
+  size_t releasedMemorySize = _nodes.back().size * sizeof(int);
+
+  delete _nodes.back().data;
+  _nodes.pop_back();
+
+  std::cout << "Popped node with memory occupation: "
+            << releasedMemorySize << std::endl;
+}
+
+void MemoryMonitor::printMemoryOccupation(const int numberOfNodes) {
+  size_t usedMemory = 0;
+  const size_t N = std::min(_nodes.size(), (size_t) numberOfNodes);
+
+  for (size_t i = 0; i < N; ++i) {
+    usedMemory += _nodes[i].size * sizeof(int);
+  }
+
+  std::cout << "Memory occupation for first " << numberOfNodes
+            << " memory nodes is: " << usedMemory << std::endl;
+}
+
+MemoryNode MemoryMonitor::getLastNode() const {
+  return _nodes.back();
+}
