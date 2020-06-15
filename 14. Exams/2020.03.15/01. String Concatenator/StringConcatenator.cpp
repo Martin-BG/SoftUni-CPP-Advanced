3efdee67dae9a886361cb52dbe0f01c028d99a65
MartinBG <MartinBG@abv.bg>
2020-06-15 10:04:56 +0300

    task #1

--- 14. Exams/2020.03.15/01. String Concatenator/StringConcatenator.cpp
+++ 14. Exams/2020.03.15/01. String Concatenator/StringConcatenator.cpp
@@ -1,3 +1,35 @@
+#include <sstream>
+#include <algorithm>
 #include "StringConcatenator.h"
 
-//TODO
+StringConcatenator::StringConcatenator() { };
+
+StringConcatenator::~StringConcatenator() { }
+
+std::string StringConcatenator::concatenate(ConcatenateStrategy strategy,
+                                            const char* left, size_t leftSize,
+                                            const char* right, size_t rightSize) const {
+  int leftStep = (strategy == ConcatenateStrategy::LEFT_2_RIGHT_1) ? 2 : 1;
+  int rightStep = (strategy == ConcatenateStrategy::LEFT_1_RIGHT_2) ? 2 : 1;
+  size_t leftIndex = 0;
+  size_t rightIndex = 0;
+
+  std::ostringstream oss{ };
+
+  while (leftIndex + leftStep <= leftSize && rightIndex + rightStep <= rightSize) {
+    std::copy_n(&left[leftIndex], leftStep, std::ostreambuf_iterator<char>(oss));
+    std::copy_n(&right[rightIndex], rightStep, std::ostreambuf_iterator<char>(oss));
+    leftIndex += leftStep;
+    rightIndex += rightStep;
+  }
+
+  if (leftIndex < leftSize) {
+    std::copy_n(&left[leftIndex], leftSize - leftIndex, std::ostreambuf_iterator<char>(oss));
+  }
+
+  if (rightIndex < rightSize) {
+    std::copy_n(&right[rightIndex], rightSize - rightIndex, std::ostreambuf_iterator<char>(oss));
+  }
+
+  return oss.str();
+}
