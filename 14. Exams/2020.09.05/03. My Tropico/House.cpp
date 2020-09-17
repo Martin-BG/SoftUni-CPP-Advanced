dd98488be9c2b1507f93790d6f84dfb773f3f3ab
MartinBG <MartinBG@abv.bg>
2020-09-17 20:47:08 +0300

    task #3

--- 14. Exams/2020.09.05/03. My Tropico/House.cpp
+++ 14. Exams/2020.09.05/03. My Tropico/House.cpp
@@ -1,3 +1,15 @@
-#include "Palace.h"
+#include "House.h"
 
-// TODO
+void House::print() {
+  std::cout << "Building type: House, measurements: " << this->getWidth() << " x " << this->getLength() << std::endl;
+}
+
+House::House(int width, int length) : Building(width, length) {
+  this->print();
+  std::cout << "| Constructor is called" << std::endl;
+}
+
+House::~House() {
+  this->print();
+  std::cout << "| Destructor is called" << std::endl;
+}
--- 14. Exams/2020.09.05/03. My Tropico/Palace.cpp
+++ 14. Exams/2020.09.05/03. My Tropico/Palace.cpp
@@ -1,3 +1,15 @@
-#include "House.h"
+#include "Palace.h"
 
-// TODO
+void Palace::print() {
+  std::cout << "Building type: Palace, measurements: " << this->getWidth() << " x " << this->getLength() << std::endl;
+}
+
+Palace::Palace(int width, int length) : Building(width, length) {
+  this->print();
+  std::cout << "| Constructor is called" << std::endl;
+}
+
+Palace::~Palace() {
+  this->print();
+  std::cout << "| Destructor is called" << std::endl;
+}
