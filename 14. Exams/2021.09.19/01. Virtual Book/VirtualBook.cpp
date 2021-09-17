a3b261ff07bd1296faf6fba819a115236d786414
MartinBG <MartinBG@abv.bg>
2021-09-17 12:59:40 +0300

    task #1

---
+++ 14. Exams/2021.09.19/01. Virtual Book/VirtualBook.cpp
@@ -0,0 +1,19 @@
+#include "VirtualBook.h"
+
+void VirtualBook::addPage(VirtualPage page) {
+  _pages.push_back(std::move(page));
+}
+
+void VirtualBook::printContent() const {
+  for (const auto& page: _pages) {
+    page.printContent();
+  }
+}
+
+void VirtualBook::removeLastPage() {
+  _pages.pop_back();
+}
+
+void VirtualBook::removeAllPages() {
+  _pages.clear();
+}
